65d85ddda61c19b54bba5464be16c67707b838bd
2byte <truesb@qq.com>
2016-06-14 18:54:24 +0800

    BETA 1.0

--- js/player.js
+++ js/player.js
@@ -1,21 +1,21 @@
 	var audio = document.getElementById("myMusic");
-
-
-	var sentence = new Array()
-	sentence[0] = "我再测试一下谷歌，因为火狐真的不好使，懒得去调哪里有问题了。还是谷歌好，不会出错。下面开始进行歌词输入。预备。冉冉升起，在分裂的天空留下足迹，生命中，最美丽的一天。哦哦哦哦哦哦。一切都是太阳的光辉，金色月亮，生命中，最美丽的一天~！！！！我再测试一下谷歌，因为火狐真的不好使，懒得去调哪里有问题了。还是谷歌好，不会出错。下面开始进行歌词输入。预备。冉冉升起，在分裂的天空留下足迹，生命中，最美丽的一天。哦哦哦哦哦哦。一切都是太阳的光辉，金色月亮，生命中，最美丽的一天~！！！！我再测试一下谷歌，因为火狐真的不好使，懒得去调哪里有问题了。还是谷歌好，不会出错。下面开始进行歌词输入。预备。冉冉升起，在分裂的天空留下足迹，生命中，最美丽的一天。哦哦哦哦哦哦。一切都是太阳的光辉，金色月亮，生命中，最美丽的一天~！！！！我再测试一下谷歌，因为火狐真的不好使，懒得去调哪里有问题了。还是谷歌好，不会出错。下面开始进行歌词输入。预备。冉冉升起，在分裂的天空留下足迹，生命中，最美丽的一天。哦哦哦哦哦哦。一切都是太阳的光辉，金色月亮，生命中，最美丽的一天~！！！！我再测试一下谷歌，因为火狐真的不好使，懒得去调哪里有问题了。还是谷歌好，不会出错。下面开始进行歌词输入。预备。冉冉升起，在分裂的天空留下足迹，生命中，最美丽的一天。哦哦哦哦哦哦。一切都是太阳的光辉，金色月亮，生命中，最美丽的一天~！！！！我再测试一下谷歌，因为火狐真的不好使，懒得去调哪里有问题了。还是谷歌好，不会出错。下面开始进行歌词输入。预备。冉冉升起，在分裂的天空留下足迹，生命中，最美丽的一天。哦哦哦哦哦哦。一切都是太阳的光辉，金色月亮，生命中，最美丽的一天~！！！！我再测试一下谷歌，因为火狐真的不好使，懒得去调哪里有问题了。还是谷歌好，不会出错。下面开始进行歌词输入。预备。冉冉升起，在分裂的天空留下足迹，生命中，最美丽的一天。哦哦哦哦哦哦。一切都是太阳的光辉，金色月亮，生命中，最美丽的一天~！！！！"
-
-	var timeTable = [2,1,100,3,200,8,300,12,400,13,833,60]
-
+	
+	var enterHeight=0;
 	var tableLenght = sentence[0].length
 	var nowHeight=10;
 	//定义一个当前行高度。
-	var halfWindowHeight = $(window).height()/2;
+	var halfWindowHeight = $(window).height()/1.5;
 	//当前浏览器窗口一半的高度
-
+	$("#wordsTable").append('&nbsp;&nbsp;&nbsp;&nbsp;');
 	for (var i = 0; i < tableLenght; i++) {
-	$("#wordsTable").append('<span style="display:none">' + sentence[0][i] + '</span>');
+	if( sentence[0][i] =="←"){
+		$("#wordsTable").append('<span style="display:none"><br>&nbsp;&nbsp;&nbsp;&nbsp;</span>');
+		enterHeight+=64;
+			}
+	else{
+		$("#wordsTable").append('<span style="display:none">' + sentence[0][i] + '</span>');
 	}
-	//实在是找不到JS，追加HTML不自动匹配<>标签的方法，只能用PHP来生成了。
+	}
 	
 	
 
@@ -42,7 +42,7 @@
 			//这里加了一个判断，对于数组前大于后的顺序，直接跳过playSentence函数，进行下一组调用。
 			
 			window.setTimeout(function(){queuePlay(i+1)},queue[i][2]);
-			console.log("跳过了",queue[i])
+			//console.log("跳过了",queue[i])
 			}
 			else{
 			playSentence(queue[i][0],queue[i][1],queue[i][2]/(queue[i][1]-queue[i][0]))
@@ -54,7 +54,7 @@
 			}
 		else{
 		clearInterval(myClock)
-		$("#bottom").html('<a href="javascript:window.location.reload()">刷新重看</a>')
+		$("#wordsTable").append('<br><a href="write.html">我也写一个试试</a>')
 		}
 	}
 	
@@ -66,16 +66,18 @@
 	//不加这个判断无法正常播放，时间会慢很多，我还不知道怎么回事
 		if(start<end)
 		{
-		$("#wordsTable").find('span:eq(' + start + ')').css('position', 'relative').fadeIn(1000);  
+		$("#wordsTable").find('span:eq(' + start + ')').fadeIn(1000);  
 		
-		if($("#wordsTable").find('span:eq(' + start + ')').offset().top>nowHeight){
+		console.log(start)
+		wordHeight=$("#wordsTable").find('span:eq(' + start + ')').offset().top
+		if(wordHeight>nowHeight){
 		
-		nowHeight=$("#wordsTable").find('span:eq(' + start + ')').offset().top
+		nowHeight=wordHeight
 		//查询每一个字的高度，如果换行，一定增加，将新的高度赋值给nowHeight
 		
 	
 		
-		//console.log(" 当前字高"+nowHeight,"窗体一半高度： "+halfWindowHeight,"滚动条距离顶部高度： "+$(window).scrollTop())
+		//console.log("哪个字: "+$("#wordsTable").find('span:eq(' + start + ')').text()+" 当前字高"+nowHeight,"窗体一半高度： "+halfWindowHeight,"滚动条距离顶部高度： "+$(window).scrollTop())
 		
 				if(nowHeight-$(window).scrollTop()>halfWindowHeight)
 				//如果行高度多于屏幕的一半，既超出了中心高度，需要进行向下滚动
@@ -83,11 +85,10 @@
 				var needScroll= nowHeight-halfWindowHeight;
 				$('html,body').animate({scrollTop: needScroll}, 1000);
 				//滚动的高度是，行的高度 减去 屏幕一半，即向下滚动了约一行的高度
-				//console.log("滚到了:"+needScroll)
+				console.log("滚到了:"+needScroll)
 				}
 		
 		}
-
 		
 		setTimeout(function(){playSentence((start +1),end,speed)}, speed);
 		}
@@ -115,23 +116,22 @@
 	function timer(){
 	time=time+0.1
 	var str=time+""
-	$("#title").text(str.substring(0,str.indexOf(".") + 2))
+	//$("#title").text(str.substring(0,str.indexOf(".") + 2))
 	
-	if($("#title").text()=='5.0'){
-		//$("#backGround").fadeIn();
+	if(str.substring(0,str.indexOf(".") + 2)%5==0){
+		
+		$("#backGround").fadeOut(2000,function () { $("#backGround").css("background-Image","url(img/1.jpg)"); });
+		$("#backGround").fadeIn(2000);
+		
 		}
 	}
 	
-	$(function(){ 
-
-	 audio.src = "06.mp3";
-	 
-	 audio.addEventListener("canplaythrough", function (){
+	function readyToGo(){
 	$('body,html').animate({'scrollTop':0},0)
-	$("#wordsTable").css('height',tableLenght/27*64+halfWindowHeight)
+	$("#wordsTable").css('height',tableLenght/27*64+halfWindowHeight+enterHeight)
 	 $("#waitingBar").fadeOut(1100)
 	 window.setTimeout(function(){play();},1000);
-	 }, false);
 	 
-	 
-    });
+	 audio. removeEventListener("canplaythrough", readyToGo)
+	}
+	
---
+++ js/write.js
@@ -0,0 +1,124 @@
+var previousCount=0;
+var theBiggest=0;
+var previousWord=null;
+var pPreviousWord=null;
+
+var str="0";
+
+
+var hasStarted=false;
+var timeTable  = new Array;
+
+var time=0;
+var myClock;
+
+var audio = document.getElementById("myMusic");
+
+
+			
+
+
+function start(){
+$('#myContent').focus()
+audio.play();
+$("#startBtn").attr("value","有事情，暂停一下")
+$("#startBtn").attr("onclick","pauseWriting()")
+myClock=setInterval("timer()",100)
+}
+
+function pauseWriting(){
+audio.pause();
+$("#startBtn").attr("value","继续写作")
+$("#startBtn").attr("onclick","start()")
+clearInterval(myClock)
+}
+
+function submit(){
+audio.pause();
+var content = document.getElementById("myContent");
+var str=content.value.replace(/\n/g, '←')
+//content.value=str+"\n\r"+timeTable
+
+clearInterval(myClock)
+$("#startBtn").remove()
+$("#watchLink").append('<a href="index.html?content='+str+'&timetable='+timeTable+'">前往参观</a>')
+}
+
+
+function timer(){
+time=time+0.1
+str=time+""
+$("#timeTable").text(str.substring(0,str.indexOf(".") + 2))
+}
+
+
+
+function wordCheck(){
+var cursortPosition=$("#myContent").get(0).selectionStart;
+var nowCount= $("#myContent").val().length;
+var nowContent=$("#myContent").val()
+var nowInput = nowContent[nowCount-1]
+var re=/^[\u4E00-\u9FA5|\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5|\u007e]+$/
+console.log("当前输入： "+nowInput,"上一个字："+previousWord," 字数： "+nowCount,"时间： "+str)
+
+
+	if(nowCount>theBiggest){
+	theBiggest=nowCount
+	}
+	//加一个最大值，为了区分输入一个中文，与删除中文前的英文
+	//这个判断可以继续完善，因为可以检测当前光标，就可以判断光标左右是否有字，来确定是否插入。
+
+
+	if(nowCount-previousCount==-1){
+		if (re.test(nowInput)) {
+			if(re.test(previousWord)){
+			console.log("删除中文")
+			}
+			else if(!re.test(pPreviousWord)){
+				if(nowCount+1==theBiggest){
+				console.log("输入了一个中文")
+				var timeArray = [nowCount,str.substring(0,str.indexOf(".") + 2)]
+				timeTable.push(timeArray)
+				}
+				else{
+				console.log("删除中文前面的符号或英文",previousWord)
+				}
+			}
+			else{
+			console.log("删除了中文前的一个东西",previousWord)
+			}
+		
+		}
+		else{
+		console.log("删除英文")
+		}
+	}
+	else if(nowCount-previousCount<-1){
+		if (re.test(nowInput)) {
+			console.log("连拼输入")
+			var timeArray = [nowCount,str.substring(0,str.indexOf(".") + 2)]
+			timeTable.push(timeArray)
+			}
+	}
+	else{
+	if (re.test(nowInput)) {
+		console.log("中文输入")
+		var timeArray = [nowCount,str.substring(0,str.indexOf(".") + 2)]
+		timeTable.push(timeArray)
+		}
+	else{
+	console.log("英文输入")
+	}
+	}
+	
+	
+	previousCount=nowCount;
+	pPreviousWord=previousWord;
+	previousWord=nowInput;
+	
+}
+
+function readyToWrite(){
+	 $("#waitingBar").fadeOut(1100)
+	 audio.removeEventListener("canplaythrough", readyToWrite)
+	}
